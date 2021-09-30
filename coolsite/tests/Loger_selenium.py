686b31924574d7db1c464f0652062d7e14195840
FireSwami <86517715+FireSwami@users.noreply.github.com>
2021-09-30 13:05:14 +0300

    Logger-test

---
+++ coolsite/tests/Loger_selenium.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+import pathlib
+from pathlib import Path
+
+url = 'https://microgreen.ferumflex.com/'
+
+dir_path = pathlib.Path.cwd()
+driver = webdriver.Firefox(executable_path=Path(dir_path, 'coolsite', "geckodriver.exe"))
+ 
+
+try:
+    driver.get(url=url)
+    # time.sleep(3)
+
+    enter_link = driver.find_element_by_xpath('/html/body/table/tbody/tr[1]/td/div/ul/li[4]/a[2]').click()
+    # time.sleep(3)
+
+    login_input = driver.find_element_by_name("username")
+    login_input.clear()
+    login_input.send_keys('12345678')
+    # time.sleep(3)
+
+    pass_input = driver.find_element_by_name("password")
+    pass_input.clear()
+    pass_input.send_keys('Testpassword1')
+    # time.sleep(3)
+
+    # enter_link = driver.find_element_by_xpath('/html/body/table/tbody/tr[1]/td/table/tbody/tr/td[2]/div/form/button').click()
+  
+
+    pass_input.send_keys(Keys.ENTER)
+    time.sleep(6)
+
+
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
