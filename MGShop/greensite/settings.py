46e83156ac452b575343fd9211372bf915f859b9
fireswami <jobforsoul@gmail.com>
2022-01-13 18:23:56 +0200

    1.1

--- MGShop/greensite/settings.py
+++ MGShop/greensite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'delete'
+SECRET_KEY = 'django-insecure-u4l2jwmw*a^@!17onw^4cdq25__73(-ita9@#sym$u^_w=n%g9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
